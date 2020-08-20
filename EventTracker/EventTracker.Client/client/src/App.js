a87cfae93225d22ff5af1efc3786eee77f4e604b
emilia98 <emilia_nedialkova@abv.bg>
2020-08-20 17:38:38 +0300

    added main routes

--- EventTracker/EventTracker.Client/client/src/App.js
+++ EventTracker/EventTracker.Client/client/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import Admin from './components/Admin/Admin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </Router>
     </div>
   );
 }
