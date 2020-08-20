686d53ca9492caa31a5aaea572166154d56f04c6
emilia98 <emilia_nedialkova@abv.bg>
2020-08-20 23:32:45 +0300

    implemented common service for admin

---
+++ EventTracker/EventTracker.Server/Services/EventTracker.Services/ICommonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EventTracker.Services
+{
+    public interface ICommonService<TEntity>
+    {
+        Task Create(TEntity entity);
+
+        IEnumerable<TEntity> GetAll(bool? withNonActive = false, int? count = null);
+
+        TEntity GetById(int id, bool? withNonActive = false);
+
+        Task Update(TEntity entity);
+
+        Task ChangeActiveStatus(TEntity entity);
+    }
+}
---
+++ EventTracker/EventTracker.Server/Services/EventTracker.Services/TagService.cs
@@ -0,0 +1,46 @@
+﻿using EventTracker.Data;
+using EventTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTracker.Services
+{
+    public class TagService : ICommonService<Tag>
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public TagService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task ChangeActiveStatus(Tag entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(Tag entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tag> GetAll(bool? withNonActive = false, int? count = null)
+        {
+            // TODO: Implement this returning specified count of elements
+            return this.dbContext.Tags.Where(t => t.IsActive == !withNonActive);
+        }
+
+        public Tag GetById(int id, bool? withNonActive = false)
+        {
+            return this.dbContext.Tags.Where(t => t.Id == id && t.IsActive == !withNonActive).FirstOrDefault();
+        }
+
+        public Task Update(Tag entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
